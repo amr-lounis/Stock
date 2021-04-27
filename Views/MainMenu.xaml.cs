6c689236eed4634dd76bce596090bd994714ca97
hackamro <lounis.amar.unity@gmail.com>
2021-04-27 21:47:25 +0100

    Signed-off-by: hackamro <lounis.amar.unity@gmail.com>

--- Views/MainMenu.xaml.cs
+++ Views/MainMenu.xaml.cs
@@ -19,37 +19,86 @@ namespace Stock.Views
 {
     public partial class MainMenu : RibbonWindow
     {
-        TableProduct_UC v_TableProducrs = new TableProduct_UC();
-        TabDynamic_UC v_TabDynamic_UC = new TabDynamic_UC();
-
         public MainMenu()
         {
             InitializeComponent();
-            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
-            UIPanel.Children.Add(v_TabDynamic_UC);
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            try
+            {
+                InitializeComponent();
+                tabDynamic.DataContext = _tabItems;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
-
+        //-------------------------------------------------------------------------------
         private void RibbonWin_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            //if (RibbonWin.SelectedIndex == 0)
-            //{
+            if (RibbonWin.SelectedIndex == 0)
+            {
 
-            //}
-            //else if (RibbonWin.SelectedIndex == 1)
-            //{
-            //    UIPanel.Children.Clear();
-            //    UIPanel.Children.Add(v_TableUsers);
-            //}
-            //else if (RibbonWin.SelectedIndex == 2)
-            //{
-            //    UIPanel.Children.Clear();
-            //    UIPanel.Children.Add(v_TableProducrs);
-            //}
-            //else
-            //{
-            //    Console.WriteLine(RibbonWin.SelectedIndex);
-            //    Console.Beep();
-            //}
+            }
         }
+        //-------------------------------------------------------------------------------
+        public void v_btn_Customer_Click(object sender, RoutedEventArgs e)
+        {
+            TableUsers_UC tu = new TableUsers_UC();
+            AddTabItem(tu, "Customer:"+cpt);
+        }
+
+        //-------------------------------------------------------------------------------
+        private void tabDynamic_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TabItem tab = tabDynamic.SelectedItem as TabItem;
+            if (tab == null) return;
+
+            Console.WriteLine(tab.Name);
+        }
+        //-------------------------------------------------------------------------------
+        private void AddTabItem(object _content, string _name)
+        {
+            TabItem tab = new TabItem();
+            tab.Header = _name;
+            string tab_name = string.Format("tab_" + cpt++);
+            Console.WriteLine("add new tab : " + tab_name);
+            tab.Name = tab_name;
+            tab.HeaderTemplate = tabDynamic.FindResource("TabHeader") as DataTemplate;
+            tab.Content = _content;
+
+            _tabItems.Add(tab);
+            tabDynamic.DataContext = null;
+            tabDynamic.DataContext = _tabItems;
+            tabDynamic.SelectedItem = tab;
+        }
+        //-------------------------------------------------------------------------------
+        private void v_btn_delete_Click(object sender, RoutedEventArgs e)
+        {
+            string tabName = (sender as Button).CommandParameter.ToString();
+            Console.WriteLine(tabName);
+            var item = tabDynamic.Items.Cast<TabItem>().Where(i => i.Name.Equals(tabName)).SingleOrDefault();
+            TabItem tab = item as TabItem;
+
+            if (tab != null)
+            {
+                if (MessageBox.Show(string.Format("Are you sure you want to close the tab '{0}'?", tab.Header.ToString()),
+                    "Close Tab", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    TabItem selectedTab = tabDynamic.SelectedItem as TabItem;
+                    tabDynamic.DataContext = null;
+                    _tabItems.Remove(tab);
+                    tabDynamic.DataContext = _tabItems;
+                }
+            }
+            if (_tabItems.Count >= 1)
+            {
+                tabDynamic.SelectedItem = _tabItems[0];
+            }
+        }
+        //-------------------------------------------------------------------------------
+        private static List<TabItem> _tabItems = new List<TabItem>();
+        static int cpt = 0;
+        //-------------------------------------------------------------------------------
     }
 }
